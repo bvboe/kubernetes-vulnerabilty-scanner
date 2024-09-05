fedb0fbe88d44704c6a9e6da57056ff31c02d343
Bjorn Boe <bvboe@pobox.com>
2024-09-05 19:01:21 -0400

    Fixed some formatting issues

--- release.sh
+++ release.sh
@@ -2,25 +2,26 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 #Version number that updates chart and images
-VERSION="0.3"
+CHART_VERSION="0.0.1"
+APP_VERSION="0.0.4"
 POD_SCANNER_REPOSITORY="bjornvb/k8s-pod-scanner"
 VULNERABILITY_COORDINATOR_REPOSITORY="bjornvb/k8s-scanner-vulnerability-coordinator"
 WEB_FRONTEND_REPOSITORY="bjornvb/k8s-scanner-web-frontend"
 
-echo Generate release $VERSION
-./doreleasecontainer.sh "$POD_SCANNER_REPOSITORY:$VERSION" "pod-scanner"
-./doreleasecontainer.sh "$VULNERABILITY_COORDINATOR_REPOSITORY:$VERSION" "vulnerability-coordinator"
-./doreleasecontainer.sh "$WEB_FRONTEND_REPOSITORY:$VERSION" "web-frontend"
+echo Generate release $APP_VERSION
+./doreleasecontainer.sh "$POD_SCANNER_REPOSITORY:$APP_VERSION" "pod-scanner"
+./doreleasecontainer.sh "$VULNERABILITY_COORDINATOR_REPOSITORY:$APP_VERSION" "vulnerability-coordinator"
+./doreleasecontainer.sh "$WEB_FRONTEND_REPOSITORY:$APP_VERSION" "web-frontend"
 
-cat k8s-vuln-scanner/values.yaml | yq eval ".podScanner.image.tag = \"${VERSION}\" | 
-                                            .vulnerabilityCoordinator.image.tag = \"${VERSION}\" 
-                                            | .webFrontend.image.tag = \"${VERSION}\"" > k8s-vuln-scanner/newvalues.yaml
+cat k8s-vuln-scanner/values.yaml | yq eval ".podScanner.image.tag = \"${APP_VERSION}\" | 
+                                            .vulnerabilityCoordinator.image.tag = \"${APP_VERSION}\" 
+                                            | .webFrontend.image.tag = \"${APP_VERSION}\"" > k8s-vuln-scanner/newvalues.yaml
 mv k8s-vuln-scanner/newvalues.yaml k8s-vuln-scanner/values.yaml
 
-cat k8s-vuln-scanner/Chart.yaml | yq eval ".appVersion=\"${VERSION}\"" > k8s-vuln-scanner/newChart.yaml
+cat k8s-vuln-scanner/Chart.yaml | yq eval ".appVersion=\"${APP_VERSION}\""  | yq eval ".version=\"${CHART_VERSION}\"" > k8s-vuln-scanner/newChart.yaml
 mv k8s-vuln-scanner/newChart.yaml k8s-vuln-scanner/Chart.yaml
 
 echo Complete and generated the following containers:
-echo "$POD_SCANNER_REPOSITORY:$VERSION"
-echo "$VULNERABILITY_COORDINATOR_REPOSITORY:$VERSION"
-echo "$WEB_FRONTEND_REPOSITORY:$VERSION"
+echo "$POD_SCANNER_REPOSITORY:$APP_VERSION"
+echo "$VULNERABILITY_COORDINATOR_REPOSITORY:$APP_VERSION"
+echo "$WEB_FRONTEND_REPOSITORY:$APP_VERSION"
