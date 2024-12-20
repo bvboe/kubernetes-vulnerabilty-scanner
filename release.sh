5371f71e236af2166822b4d1b7174dd4ada0b1af
Bjorn Boe <bvboe@pobox.com>
2024-12-20 14:17:43 -0500

    Updated images

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 #Version number that updates chart and images
-CHART_VERSION="0.1.43"
-APP_VERSION="0.1.43"
+CHART_VERSION="0.1.45"
+APP_VERSION="0.1.45"
 POD_SCANNER_REPOSITORY="bjornvb/k8s-pod-scanner"
 VULNERABILITY_COORDINATOR_REPOSITORY="bjornvb/k8s-scanner-vulnerability-coordinator"
 WEB_FRONTEND_REPOSITORY="bjornvb/k8s-scanner-web-frontend"
